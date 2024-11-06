1d7d061b4d78d3273f0fab1ea35695726a902a6e
Matheus Novais <mthscampos@gmail.com>
2024-11-06 17:21:04 -0300

    corrigido prop 'crossOrigin'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5911231162501990"
-          crossorigin="anonymous"
+          crossOrigin="anonymous"
         ></script>
         <meta name="google-adsense-account" content="ca-pub-5911231162501990" />
       </Head>
